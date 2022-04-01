0edda533200ff7994f1ffa1df038db0bc76cd10a
VivifyIdeas <vivify@VivifyIdeass-MacBook-Pro-8.local>
2022-04-01 18:29:53 +0200

    kloniran dragov kod, nadalje, moj rad

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::resource('cars', CarController::class)->except(['create', 'edit']);
 
 // Route::get('cars', [CarController::class, 'index']);
